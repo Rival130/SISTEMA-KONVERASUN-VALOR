0aaad04c978c8811d54c6787e36f733170fd742c
pedrix-48 <jessua.belper@gmail.com>
2024-05-01 06:28:16 -0700

    aumenta icon ba menu-top

--- source/menu_top.php
+++ source/menu_top.php
@@ -8,6 +8,7 @@
        <meta name="description" content="">
        <meta name="viewport" content="width=device-width, initial-scale=1">
        <link rel="stylesheet" href="../css/bootstrap.min.css">
+       <link rel="stylesheet" href="../icons/font/bootstrap-icons.css">
        <style>
            a {
                margin-left: 15px;
@@ -22,10 +23,10 @@
                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                       <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Baranda</a></li>
-                       <li class="nav-item"><a class="nav-link active" aria-current="page" href="about.php">Introdusaun</a></li>
-                       <li class="nav-item"><a class="nav-link active" aria-current="page" href="Sistema.php">Sistema</a></li>
-                       <li class="nav-item"><a class="nav-link active" aria-current="page" href="team.php">Ekipa</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php"><i class="bi bi-house me-2"></i>Baranda</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="about.php"><i class="bi bi-info-circle me-2"></i>Introdusaun</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="Sistema.php"><i class="bi bi-menu-button-wide me-2"></i>Sistema</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="team.php"><i class="bi bi-people-fill me-2"></i>Ekipa</a></li>
                    </ul>
                </div>
            </div>
